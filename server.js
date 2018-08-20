62037697636371149147bd9ee59a6f1e4ab30447
ags1773 <amogh1773@gmail.com>
2018-08-20 21:17:56 +0530

    Exporting server, running it from app.js

--- server.js
+++ server.js
@@ -1,53 +1,51 @@
 const net = require('net')
-const port = 3000
-const timeout = 2000 // timer after which server closes the socket
+const timeout = 10 // timer after which server closes the socket
 let timeoutId
-const server = net.createServer()
-const fs = require('fs')
 
-server.on('connection', (socket) => {
-  let gotData = false
-  console.log('Client connected' + socket.remoteAddress + ':' + socket.remotePort)
+function start (port) {
+  const server = net.createServer()
+  server.on('connection', (socket) => {
+    let gotData = false
+    let body = ''
+    console.log('Client connected' + socket.remoteAddress + ':' + socket.remotePort)
 
-  socket.on('data', (chunk) => {
-    // console.log(`Data recieved`, chunk.toString('utf-8'))
-    if (!gotData) { // write response, close socket after timeout
-      gotData = true
-      fs.appendFile('dump.txt', chunk, (err) => console.log(err))
-      socket.write('HTTP/1.1 200 OK\r\n')
-      socket.write('Date: ' + (new Date()).toString() + '\r\n')
-      socket.write('Connection: close\r\n')
-      socket.write('Content-Type: text/plain\r\n')
-      socket.write('\r\n')
-      // socket.write(chunk.toString())
-      // socket.write('\r\n')
-      socket.write('First chunk recieved\r\n')
-      console.log('First chunk recieved')
-      timeoutSet(() => socket.end('Closing socket connection...\r\n'))
-    } else { // if there are more incomming chunks, write them to response and reset the timeout
-      // socket.write(chunk.toString())
-      // socket.write('\r\n')
-      fs.appendFile('dump.txt', chunk, (err) => console.log(err))
-      console.log('Chunk recieved')
-      socket.write('Chunk recieved\r\n')
-      timeoutreset(() => socket.end('Closing socket connection...\r\n'))
-    }
+    socket.on('data', (chunk) => {
+      body += chunk
+      if (!gotData) { // write response, close socket after timeout
+        gotData = true
+        socket.write('HTTP/1.1 200 OK\r\n')
+        socket.write('Date: ' + (new Date()).toString() + '\r\n')
+        socket.write('Connection: close\r\n')
+        socket.write('Content-Type: text/plain\r\n')
+        socket.write('\r\n')
+        timeoutSet(() => socket.end('Closing socket connection from server side...\r\n'))
+      } else {
+        timeoutreset(() => socket.end('Closing socket connection from server side...\r\n'))
+      }
+    })
+
+    socket.on('end', () => {
+      console.log('FIN packet recieved')
+      console.log('Body => ', body.toString('utf-8'))
+    })
+    socket.on('close', () => console.log('Socket connection closed by client'))
+    socket.on('error', (err) => console.error(err))
   })
 
-  socket.on('end', () => console.log('FIN packet recieved'))
-  socket.on('close', () => console.log('Socket connection closed by client'))
-  socket.on('error', (e) => console.error(e))
-})
-
-server.listen(port, () => console.log('Server started on port ' + port))
+  server.listen(port)
+  server.on('error', (err) => console.error(err))
+  server.on('listening', () => console.log(`Listening on port ${port}`))
+}
 
 function timeoutSet (callback) {
-  console.log('Timeout SET')
   timeoutId = setTimeout(callback, timeout)
 }
 
 function timeoutreset (callback) {
-  console.log('Timeout RESET')
   clearTimeout(timeoutId)
   timeoutId = setTimeout(callback, timeout)
+}
+
+module.exports = {
+  start
 }
