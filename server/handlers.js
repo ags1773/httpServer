9c49bc4f6f39aa923668095d6e1290a2e4c4e548
ags1773 <amogh1773@gmail.com>
2018-08-29 23:30:57 +0530

    slight improvement to staticFileHandler function

--- server/handlers.js
+++ server/handlers.js
@@ -4,20 +4,18 @@ const fs = require('fs')
 
 function staticFileHandler (req, res, next, dir) {
   console.log('%%% Running Static File handler %%%')
-  fs.readdir(dir, (err, files) => {
-    console.log('Serving Files => ', files)
-    if (err) res.setStatus(404).write(err.message).send()
-    else {
-      files.forEach(file => {
-        server.addRoute('GET', `/${file}`, (request, response) => {
-          response.body = fs.readFileSync(path.join(dir, file))
-          response.setContentType(path.extname(file).slice(1))
-          response.send()
-        })
+  try {
+    fs.readdirSync(dir).forEach(file => {
+      server.addRoute('GET', `/${file}`, (request, response) => {
+        response.body = fs.readFileSync(path.join(dir, file))
+        response.setContentType(path.extname(file).slice(1))
+        response.send()
       })
-      next(req, res)
-    }
-  })
+    })
+    next(req, res)
+  } catch (err) {
+    res.setStatus(404).write(err.message).send()
+  }
 }
 
 module.exports = {
