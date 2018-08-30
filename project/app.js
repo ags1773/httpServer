2da1daf1b32df72fde5d18940e68265f4d26b0f3
ags1773 <amogh1773@gmail.com>
2018-08-30 15:29:04 +0530

    [project] minor improvement

--- project/app.js
+++ project/app.js
@@ -13,31 +13,33 @@ server.addRoute('GET', '/', (req, res) => {
   fs.readFile(path.join(__dirname, '/todos.txt'), (err, data) => {
     if (err) res.setStatus(404).write(err.message).send()
     else {
-      let content = 'No To-Dos to show'
-      if (data) {
-        let LIs = data.toString().split('\r\n').map(e => '<li>' + e + '</li>').join('')
-        content = `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>To Do List</title>
-          <link rel="stylesheet" href="/style.css">
-        </head>
-        <body>
-          <form action="/" method="post">
-            <input type="text" placeholder="To-Do">
-            <button type="submit">Add Todo</button>
-          </form>
-          <div>
-            My To-Dos:
-            <ul>` + LIs + `</ul>
-            </div>
-            <script src="/script.js"></script>
-          </body>
-          </html>`
+      let LIs = '<li>No To-Dos to show</li>'
+      if (data && data.length) {
+        // console.log('DATA >>>>', data.toString().split('\r\n').splice(-1))
+        console.log('DATA >>>>', data.toString().split('\r\n').slice(0, -1).map(e => '<li>' + e + '</li>').join(''))
+        LIs = data.toString().split('\r\n').slice(0, -1).map(e => '<li>' + e + '</li>').join('')
       }
+      let content = `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>To Do List</title>
+        <link rel="stylesheet" href="/style.css">
+      </head>
+      <body>
+        <form action="/" method="post">
+          <input type="text" placeholder="To-Do">
+          <button type="submit">Add Todo</button>
+        </form>
+        <div>
+          My To-Dos:
+          <ul>` + LIs + `</ul>
+          </div>
+          <script src="/script.js"></script>
+        </body>
+        </html>`
       res.write(content).setStatus(200).setContentType('html').send()
     }
   })
